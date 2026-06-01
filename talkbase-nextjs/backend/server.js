d0fb6ee58efa74daa7575b765add2102d345ef99
RahabMalik <rahabmalik2004@gmail.com>
2026-06-01 13:18:20 +0500

    fix cors x-api-key header

--- talkbase-nextjs/backend/server.js
+++ talkbase-nextjs/backend/server.js
@@ -15,7 +15,7 @@ app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", origin);
   }
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization,x-api-key");
   res.setHeader("Access-Control-Allow-Credentials", "true");
   if (req.method === "OPTIONS") return res.status(200).end();
   next();
